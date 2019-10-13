5fc9b03f602378f102e141c24480b1b62e21edc7
Shamel Lakin <christlife4u@gmail.com>
2019-10-13 14:45:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      "reject"
+      @status = "reject"
       "Transaction rejected. Please check your account balance."
     end
   end 
